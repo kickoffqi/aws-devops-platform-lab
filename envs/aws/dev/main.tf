2271031ff3c87def628e56ccb11ab0e870da5536
Jacob Qi <qiyu0001@gmail.com>
2026-02-09 13:38:51 +1100

    Add VPC module configuration to main Terraform file

--- envs/aws/dev/main.tf
+++ envs/aws/dev/main.tf
@@ -2,3 +2,14 @@ locals {
   project = var.project
   env     = var.env
 }
+
+module "vpc" {
+  source = "../../../modules/vpc"
+
+  project = var.project
+  env     = var.env
+
+  vpc_cidr             = var.vpc_cidr
+  public_subnet_cidrs  = var.public_subnet_cidrs
+  private_subnet_cidrs = var.private_subnet_cidrs
+}
